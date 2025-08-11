5b668b8c9452340ff870951b3b4463428fbf1d9e
George Knaggs <george_knaggs@yahoo.com>
2025-08-10 20:28:48 -0500

    Initial Commit

---
+++ Rideshare Tracker/Rideshare_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rideshare_TrackerApp.swift
+//  Rideshare Tracker
+//
+//  Created by George on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Rideshare_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rideshare TrackerTests/Rideshare_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rideshare_TrackerTests.swift
+//  Rideshare TrackerTests
+//
+//  Created by George on 8/10/25.
+//
+
+import Testing
+@testable import Rideshare_Tracker
+
+struct Rideshare_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
